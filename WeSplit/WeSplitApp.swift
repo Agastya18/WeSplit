6faab23691104dbbf950bcc53fdc4e96e8fc0831
agastya gaur <agastyagaur17@gmail.com>
2024-03-28 21:00:39 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Agastya Gaur on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
